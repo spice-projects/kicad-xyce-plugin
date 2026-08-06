40d620203bdf38cc0e2b089082d93d77a2a34a51
Rogelio J. Baucells <rogelio.javier@baucells.net>
2026-08-05 22:28:47 -0400

    Implement FFT calculation orchestration using PocketFFT

---
+++ src/fft/fft.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <optional>
+#include <span>
+#include <vector>
+
+namespace fft
+{
+    enum class WindowFunction
+    {
+        RECTANGULAR,
+        HAMMING,
+        HANNING,
+        BLACKMAN
+    };
+
+    enum class FftOutput
+    {
+        MAGNITUDE,
+        MAGNITUDE_DB,
+        PHASE
+    };
+
+    enum class InterpolationAlgorithm
+    {
+        LINEAR,
+        PCHIP,
+        CUBIC_SPLINE
+    };
+
+    struct FftResult
+    {
+        std::vector<double> frequencies;
+        std::vector<std::vector<double>> values;
+    };
+
+    // computes fft for many real-valued signals that share the same time-domain abscissa
+    FftResult compute_fft_many2(std::span<const double> x, const std::vector<std::span<const double>>& y_matrix, double max_frequency, WindowFunction window = WindowFunction::RECTANGULAR, bool normalize = false, size_t x_left_index = 0, std::optional<size_t> x_right_index = std::nullopt, FftOutput output = FftOutput::MAGNITUDE, bool keep_dc = false, InterpolationAlgorithm algo = InterpolationAlgorithm::LINEAR);
+} // namespace fft
