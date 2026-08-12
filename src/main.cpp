2a8db370e6798da10965a147c571cddad30839a5
Rogelio J. Baucells <rogelio.javier@baucells.net>
2026-08-12 09:03:41 -0400

    Initial PoC

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-#include "app.h"
+// #include "app.h"
+// #include <main-window.h>
+#include <slint.h>
+#include <add-plot-dialog.h>
 
 // implement wxApp entry point
-wxIMPLEMENT_APP(App); // NOLINT
+// wxIMPLEMENT_APP(App); // NOLINT
+
+int main(int argc, char** argv) {
+
+    // auto ui = MainWindow::create();
+    // auto ui = FftDialog::create();
+    auto ui = AddPlotDialog::create();
+
+    // ui->on_request_increase_value([&] { ui->set_counter(ui->get_counter() + 1); });
+
+    ui->show();
+
+    slint::run_event_loop();
+}
